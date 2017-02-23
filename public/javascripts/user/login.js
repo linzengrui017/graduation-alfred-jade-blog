95c7dab5ca9af78299db50e953f2373505260123
linzengrui <1138504403@qq.com>
2017-02-23 15:53:11 +0800

    login form

---
+++ public/javascripts/user/login.js
@@ -0,0 +1,81 @@
+/**
+ * Created by lzr on 2017/2/23.
+ */
+
+/**
+ * 弹出框默认隐藏
+ */
+$("#alertPopover").hide();
+
+// function showPopover() {
+//     $('#alertPopover').append(
+//         '<div role="alert" class="alert alert-danger alert-dismissible fade in">'+
+//             '<button type="button" data-dismiss="alert" aria-label="Close" class="close"><span aria-hidden="true">×</span></button>'+
+//             '<center id="message"></center>'+
+//         '</div>');
+// }
+
+/**
+ * 登录按钮
+ * 前端校验
+ */
+$("#btn_login").click(function () {
+    var login_username = $('#login_username').val();
+    if(null == login_username || "" == login_username){
+        $('#alertPopover').show();  //弹出提示框
+        // showPopover();
+        $('#message').text('用户名不能为空');     //设置提示信息
+        return false;   //当click事件的返回值是false,阻止submit事件
+    }
+    // if(login_username.length > 20){
+    //     $('#alertPopover').show();
+    //     $('#message').text('用户名的长度不能大于20');
+    //     return false;
+    // }
+
+    var login_password = $('#login_password').val();
+    if(null == login_password || "" == login_password){
+        $('#alertPopover').show();
+        $('#message').text('密码不能为空');
+        return false;
+    }
+    // if(login_password.length > 20){
+    //     $('#alertPopover').show();
+    //     $('#message').text('密码的长度不能大于20');
+    //     return false;
+    // }
+
+
+});
+
+
+/**
+ * 注册按钮
+ * 前端校验
+ */
+
+$('#btn_reg').click(function () {
+    var reg_username = $('#reg_username').val();
+    if (null == reg_username || "" == reg_username){
+
+        return false;
+    }
+
+    var reg_pwd1 = $('#reg_pwd1').val();
+    if (null == reg_pwd1 || "" == reg_pwd1){
+
+        return false;
+    }
+
+    var reg_pwd2 = $('#reg_pwd2').val();
+    if (null == reg_pwd2 || "" == reg_pwd2){
+
+        return false;
+    }
+
+    if (reg_pwd1 != reg_pwd2){
+
+        return false;
+    }
+
+});
